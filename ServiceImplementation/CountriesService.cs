c4f10827bb91ea8467693e48d1f3df7cef6ed7c7
MBoota <m43577535@gmail.com>
2024-01-08 18:00:05 +0500

    Fixing All Error By AgrumentNullExaption and ArgumentExeption and then  XUnite Test working ok

--- ServiceImplementation/CountriesService.cs
+++ ServiceImplementation/CountriesService.cs
@@ -13,10 +13,20 @@ namespace ServiceImplementation
         }
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            //=== Null Validation ===
+            //=== 1. Null Validation ===
             if (countryAddRequest == null)
             {
                 throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+            //=== 2. Null Name Validation ===
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName.ToString()));
+            }
+            //=== 2. dublicate Validation ===
+            if (_countries.Where(temp=>temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given CountryName alredy Exist in Table");
             }
 
             //convert CountryRequest to Country Object
