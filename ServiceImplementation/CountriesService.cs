adfc6096c6228b249013492471dc9b8e38bd3d36
MBoota <m43577535@gmail.com>
2024-01-23 17:51:02 +0500

    Adding Complete Add logic for country

--- ServiceImplementation/CountriesService.cs
+++ ServiceImplementation/CountriesService.cs
@@ -1,13 +1,33 @@
-﻿using Services;
+﻿using Entities;
+using Services;
 using Services.DTO;
 
 namespace ServiceImplementation
 {
     public class CountriesService : ICountiesService
     {
+        private readonly List<Country> _countries;
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            return new CountryResponse() { CountryName = countryAddRequest.CountryName };
+            //return new CountryResponse() { CountryName = countryAddRequest.CountryName };
+
+            //convert CountryRequest to Country Object
+            Country country = countryAddRequest.ToCountry();
+
+            //generate countryId
+            country.CountryId = Guid.NewGuid();
+
+            //Add county object int _country
+            _countries.Add(country);
+
+            //convert country to countryResponse object
+            return country.ToContryResponse();
+
+
         }
     }
 }
