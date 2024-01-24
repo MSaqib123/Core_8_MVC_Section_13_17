f077ba64b44c153c414101c1d532bbc732b4ece0
MBoota <m43577535@gmail.com>
2024-01-24 17:06:39 +0500

    Testing  CountryService by XUnit

--- TestXUnit/CountriesServiceTest.cs
+++ TestXUnit/CountriesServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using Services;
+﻿using ServiceImplementation;
+using Services;
+using Services.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +12,74 @@ namespace TestXUnit
     public class CountriesServiceTest
     {
         private readonly ICountiesService _countiesService;
-        public CountriesServiceTest(ICountiesService countiesService)
+        public CountriesServiceTest()
         {
-            _countiesService = countiesService;
+            _countiesService = new CountriesService();
         }
-        //========= 1. Arrange =============
+        //=============================================================
+        //============== 1. CountryAddResult Is Null ==================
+        //============================================================= 
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            //Arrange
+            CountryAddRequest? Req = null;
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _countiesService.AddCountry(Req);
+            });
+        }
+        //=============================================================
+        //============== 2. CountryName Is Null ==================
+        //============================================================= 
+        [Fact]
+        public void AddCountry_NullCountryName()
+        {
+            //Arrange
+            CountryAddRequest? Req = new CountryAddRequest() { CountryName = null };
 
-        //========= 2. Act =============
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _countiesService.AddCountry(Req);
+            });
+        }
 
-        //========= 3. Assert =============
+        //=============================================================
+        //============== 3. CountryName Is dublicate ==================
+        //============================================================= 
+        [Fact]
+        public void AddCountry_NullCountryDublicate()
+        {
+            //Arrange
+            CountryAddRequest? Req1 = new CountryAddRequest() { CountryName = "Pakistan" };
+            CountryAddRequest? Req2 = new CountryAddRequest() { CountryName = "Pakistan" };
 
-        //--- View => TestExplorer(CTR+E+T) => Run => se result
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _countiesService.AddCountry(Req1);
+                _countiesService.AddCountry(Req2);
+            });
+        }
+
+        //=============================================================
+        //============== 4. supply proper country name ==================
+        //============================================================= 
+        [Fact]
+        public void AddCountry_NullCountryDetail()
+        {
+            //Arrange
+            CountryAddRequest? Req1 = new CountryAddRequest() { CountryName = "Japan" };
+
+            //act
+            CountryResponse resposne = _countiesService.AddCountry(Req1);
+
+            //Assert
+            Assert.True(resposne.CountryId != Guid.Empty);
+        }
+
     }
+
 }
