d08936e68fa1da419e43d6029b0495fe93797001
MBoota <m43577535@gmail.com>
2024-02-08 23:33:17 +0500

    3rd way to get value from JSON

--- Config_to_EntityFrameworkCore/Program.cs
+++ Config_to_EntityFrameworkCore/Program.cs
@@ -11,11 +11,15 @@ app.UseEndpoints(endP =>
     {
         //___________ 1. Way 1 Getting Value from appsettng.JSON______
         string s1 = app.Configuration["MyKey"];
-        await context.Response.WriteAsync(s1);
+        await context.Response.WriteAsync(s1 + " \n");
 
         //___________ 2. Way 2 Getting Value from appsettng.JSON______
         string s2 = app.Configuration.GetValue<string>("MyKey");
-        await context.Response.WriteAsync(s2);
+        await context.Response.WriteAsync(s2 + " \n");
+
+        //___________ 3. Way 3 Getting Value from appsettng.JSON______
+        string s3 = app.Configuration.GetValue<string>("X","DefaultValue_ifNotPressent");
+        await context.Response.WriteAsync(s3 + " \n");
 
     });
 });
