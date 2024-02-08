0e9309667690109e4d337254a19ec813f6ad3821
MBoota <m43577535@gmail.com>
2024-02-09 02:11:34 +0500

    getting Configuration using Cntroller

--- Config_to_EntityFrameworkCore/Controllers/HomeController.cs
+++ Config_to_EntityFrameworkCore/Controllers/HomeController.cs
@@ -20,6 +20,38 @@ namespace Config_to_EntityFrameworkCore.Controllers
         #endregion
 
 
-        
+
+        //=========================================================================
+        //========================== Section = Configuration (Importent) ==========================
+        //=========================================================================
+
+        //___ By Program.cs ___
+        #region Configuration_in_ProgramFile
+        //Please go to program file
+        #endregion
+
+        //___ by controller DI ___
+        #region Configuration_byController
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //__________ 1st way _________
+            ViewBag.MyKey1 = _configuration["MyKey"];
+
+            //__________ 2nd way _________
+            ViewBag.MyKey2 = _configuration.GetValue<string>("MyKey");
+
+            //__________ 3rd way _________
+            ViewBag.MyKey3 = _configuration.GetValue("MyKeyss", "Pakistan");
+
+            return View();
+        }
+        #endregion
+
     }
 }
