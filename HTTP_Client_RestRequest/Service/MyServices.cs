959d970a2bfe8085aff5e4b8d6bc880a8212d3ea
MBoota <m43577535@gmail.com>
2024-02-09 23:44:38 +0500

    Create HttpContext Service To get Stock marketing prices list

--- HTTP_Client_RestRequest/Service/MyServices.cs
+++ HTTP_Client_RestRequest/Service/MyServices.cs
@@ -1,7 +1,26 @@
-﻿namespace HTTP_Client_RestRequest.Service
+﻿using System.Net.NetworkInformation;
+
+namespace HTTP_Client_RestRequest.Service
 {
     public class MyServices
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MyServices(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
+        public async Task method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get,
+                };
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            }
+        }
     }
 }
