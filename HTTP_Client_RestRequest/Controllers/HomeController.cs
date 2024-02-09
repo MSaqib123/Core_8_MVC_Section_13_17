2c0c5f24b66d2c4c05b67642421781589d41ce54
MBoota <m43577535@gmail.com>
2024-02-09 22:27:58 +0500

    Adding httpClient project

---
+++ HTTP_Client_RestRequest/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using HTTP_Client_RestRequest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HTTP_Client_RestRequest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HTTP_Client_RestRequest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTTP_Client_RestRequest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
