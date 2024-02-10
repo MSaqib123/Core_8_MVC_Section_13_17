95ce22bcc7aef776d988bcbbc828afb83389911a
MBoota <m43577535@gmail.com>
2024-02-10 15:35:36 +0500

    Testing Complete MyMath unit dumyclass

--- TestXUnit/UnitTest1.cs
+++ TestXUnit/UnitTest1.cs
@@ -5,7 +5,18 @@ namespace TestXUnit
         [Fact]
         public void Test1()
         {
+            //========= 1. Arrange =============
+            MyMath m = new MyMath();
+            int a = 10, b = 5;
+            int expectValue = 15;
 
+            //========= 2. Act =============
+            int actual = m.Add(a,b);
+
+            //========= 3. Assert =============
+            Assert.Equal(expectValue,actual);
+
+            //--- View => TestExplorer(CTR+E+T) => Run => se result
         }
     }
 }
