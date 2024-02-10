3696399450d2b076bcc4d4dca41e6ed284db9d68
MBoota <m43577535@gmail.com>
2024-02-10 15:42:24 +0500

    Adding country Moedl

--- Entities/Countries.cs
+++ Entities/Countries.cs
@@ -2,7 +2,13 @@
 {
     public class Countries
     {
+        /// <summary>
+        /// Domain Model for storing Country Data
+        /// </summary>
         public Guid CountryId { get; set; }
         public string CountryName { get; set; }
+        //We do not  show Domain Model to  Controller  not in argument not in return
+        //we will  use DTO for Request and Response   
+        //between   Request && Respone DTO  we will map  that values to  domain Model
     }
 }
