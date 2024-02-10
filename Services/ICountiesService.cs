5b88599277a639ab31eaab69866ef6b5db3cf937
MBoota <m43577535@gmail.com>
2024-02-10 16:28:51 +0500

    Adding INterface Method for DI

--- Services/ICountiesService.cs
+++ Services/ICountiesService.cs
@@ -1,7 +1,12 @@
-﻿namespace Services
-{
-    public class ICountiesService
-    {
+﻿using Services.DTO;
 
+namespace Services
+{
+    /// <summary>
+    /// Represent  BusniceLogic  DI for  Country
+    /// </summary>
+    public interface ICountiesService
+    {
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
     }
 }
