e6521f89d56e24a3af214b43908f533e99c11879
MBoota <m43577535@gmail.com>
2024-02-12 00:31:35 +0500

    Adding PersonAddRequest

---
+++ Services/DTO/PersonAddRequest.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO
+{
+    /// <summary>
+    /// DTO class to add new Country
+    /// </summary>
+    public class PersonAddRequest
+    {
+        //we don not add Id in  Request
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool? ReceiveNewLetters { get; set; }
+
+        //___ Convert CountryRequest to CountryModel ___
+        public Person ToPerson()
+        {
+            return new Person()
+            {
+                PersonName = PersonName,
+                Email = Email,
+                DateOfBirth = DateOfBirth,
+                Gender = Gender,
+                CountryID = CountryID,
+                Address = Address,
+                ReceiveNewLetters = ReceiveNewLetters
+            };
+        }
+    }
+}
