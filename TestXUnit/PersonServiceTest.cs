c5b67a3b79ef21f244e907a1ecad344d42285eda
MBoota <m43577535@gmail.com>
2024-02-13 23:59:13 +0500

    bakarat admee koi comment ni kra aaaj

---
+++ TestXUnit/PersonServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestXUnit
+{
+    public class PersonServiceTest
+    {
+        //Pakistan
+        public int personName { get; set; }
+    }
+}
