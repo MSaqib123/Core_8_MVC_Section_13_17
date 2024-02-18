73189a7aa8e1129deeabdcf408301b60374cc254
MBoota <m43577535@gmail.com>
2024-02-18 18:26:33 +0500

    sir ka dard ha ya xUnit testing .  salleee

--- TestXUnit/PersonServiceTest.cs
+++ TestXUnit/PersonServiceTest.cs
@@ -134,5 +134,86 @@ namespace TestXUnit
 
         #endregion
 
+        //________ Get Person All Person ___________
+        #region GetAllPerson
+        [Fact]
+        public void GetAllPerson_EmptyList()
+        {
+            //act
+            List<PersonResponse> person_from_get = _personService.GetAllPersons();
+
+            //assert
+            Assert.Empty(person_from_get);
+
+        }
+        [Fact]
+        public void GetAllPerson_AddFewPerson()
+        {
+            //Arrange
+            CountryAddRequest country_request_1 = new CountryAddRequest() { CountryName = "Pakistan_1" };
+            CountryAddRequest country_request_2 = new CountryAddRequest() { CountryName = "Pakistan_2" };
+            CountryResponse countryResponse_1 =  _countryService.AddCountry(country_request_1);
+            CountryResponse countryResponse_2 = _countryService.AddCountry(country_request_2);
+
+            PersonAddRequest personAddRequest_1 = new PersonAddRequest()
+            {
+                PersonName = "Person name 1",
+                Email = "email@sample.com",
+                DateOfBirth = DateTime.Now,
+                Gender = GenderOptions.Male,
+                CountryID = countryResponse_1.CountryId,
+                Address = "Pakistan",
+                ReceiveNewLetters = false,
+            };
+            PersonAddRequest personAddRequest_2 = new PersonAddRequest()
+            {
+                PersonName = "Person name 2",
+                Email = "email@sample.com",
+                DateOfBirth = DateTime.Now,
+                Gender = GenderOptions.Male,
+                CountryID = countryResponse_2.CountryId,
+                Address = "Pakistan",
+                ReceiveNewLetters = false,
+            };
+            PersonAddRequest personAddRequest_3 = new PersonAddRequest()
+            {
+                PersonName = "Person name 2",
+                Email = "email@sample.com",
+                DateOfBirth = DateTime.Now,
+                Gender = GenderOptions.Male,
+                CountryID = countryResponse_2.CountryId,
+                Address = "Pakistan",
+                ReceiveNewLetters = false,
+            };
+
+            //PersonResponse p1 = _personService.AddPerson(personAddRequest_1);
+            //PersonResponse p2 = _personService.AddPerson(personAddRequest_2);
+            //PersonResponse p3 = _personService.AddPerson(personAddRequest_3);
+            List<PersonAddRequest> person_requst_addList = new List<PersonAddRequest>()
+            {
+                personAddRequest_1 , personAddRequest_2 , personAddRequest_3
+            };
+
+            List<PersonResponse> personResponses_List = new List<PersonResponse>();
+
+            foreach (PersonAddRequest item in person_requst_addList)
+            {
+                 PersonResponse person_response = _personService.AddPerson(item);
+                personResponses_List.Add(person_response);
+            }
+
+            //act
+            List<PersonResponse> personList_from_get = _personService.GetAllPersons();
+
+            //assert
+            foreach (PersonResponse person_response_from_add in personList_from_get)
+            {
+                Assert.Contains(person_response_from_add, personList_from_get);
+            }
+
+            //sir ka Dard ha ya to
+        }
+
+        #endregion
     }
 }
