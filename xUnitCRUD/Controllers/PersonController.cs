b6bd8be5317102e63b216bb45f76fe1502d5edaa
MBoota <m43577535@gmail.com>
2024-02-25 17:45:31 +0500

    Adding Searching and searchby functionality using searvices

---
+++ xUnitCRUD/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.DTO;
+
+namespace xUnitCRUD.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonService _IPersonService;
+        public PersonController(IPersonService IPersonService)
+        {
+            _IPersonService = IPersonService;
+        }
+
+        public IActionResult PersonList(string? searchBy,string? searchString)
+        {
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                {nameof(PersonResponse.PersonName),"PersonName" },
+                {nameof(PersonResponse.Email),"Email" },
+                {nameof(PersonResponse.Address),"Address" },
+                {nameof(PersonResponse.Gender),"Gender" },
+                {nameof(PersonResponse.DateOfBirth),"DateOfBirth" },
+                {nameof(PersonResponse.CountryID),"CountryID" },
+            };
+            List<PersonResponse> response = _IPersonService.GetAllPersons();
+            //___ Filter Records ___
+            if (!string.IsNullOrEmpty(searchBy) && !string.IsNullOrEmpty(searchString))
+            {
+                response = _IPersonService.GetFilteredPersons(searchBy,searchString);
+            }
+            return View(response);
+        }
+    }
+}
--- xUnitCRUD/Program.cs
+++ xUnitCRUD/Program.cs
@@ -1,7 +1,13 @@
+using ServiceImplementation;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IPersonService, PersonService>();
+builder.Services.AddSingleton<ICountiesService, CountriesService>();
 
 var app = builder.Build();
 
